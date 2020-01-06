85fdaa2809463eefd8825f31c00e47e3ecab76fc
Masiu <ciszekmarcell@gmail.com>
2020-01-06 11:50:45 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteMetadata: {
+    title: `Worlds Best`,
+    description: `Worlds best food, with passion and love`,
+    author: `@masiuciszek`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `worlds best`,
+        short_name: `worlds`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import SEO from "../components/seo"
+
+const IndexPage: React.FC = () => (
+  <>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <h1>apa</h1>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
+  </>
+)
+
+export default IndexPage
