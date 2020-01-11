20af2c78a654712cef627a8afaf2babbb9e0b28b
Masiu <ciszekmarcell@gmail.com>
2020-01-11 13:27:25 +0100

    query all images

--- src/components/home/FoodGrid/FoodGrid.tsx
+++ src/components/home/FoodGrid/FoodGrid.tsx
@@ -3,17 +3,50 @@ import { StyledFoodGrid, GridItem } from './Styles.foodgrid';
 import { useStaticQuery, graphql } from 'gatsby';
 import Img, { FluidObject } from 'gatsby-image';
 
-const queryData = graphql`
+const SINGLE_IMG = graphql`
   query {
-    img: file(relativePath: { eq: "homeGallery/img-1.jpeg" }) {
+    img: file(relativePath: { eq: "homeGallery/dessert2.jpg" }) {
       childImageSharp {
-        fluid(maxWidth: 500) {
+        fluid(maxWidth: 400) {
+          ...GatsbyImageSharpFluid_tracedSVG
+          # ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        }
+      }
+    }
+    img2: file(relativePath: { eq: "homeGallery/egg.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 400) {
+          ...GatsbyImageSharpFluid_tracedSVG
+          # ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        }
+      }
+    }
+    img3: file(relativePath: { eq: "homeGallery/oats.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 400) {
           ...GatsbyImageSharpFluid_tracedSVG
           # ...GatsbyImageSharpFluid_withWebp_tracedSVG
         }
       }
     }
   }
+
+  #   query{
+  #   images:allFile(filter:{relativeDirectory:{eq:"homeGallery"}}){
+  #     edges{
+  #       node{
+  #         name
+  #         sourceInstanceName
+  #         relativePath
+  #         childImageSharp{
+  #           fluid(maxWidth: 500){
+  #             src
+  #           }
+  #         }
+  #       }
+  #     }
+  #   }
+  # }
 `;
 
 interface Props {
@@ -22,15 +55,35 @@ interface Props {
       fluid?: FluidObject | FluidObject[] | undefined;
     };
   };
+  img2: {
+    childImageSharp: {
+      fluid?: FluidObject | FluidObject[] | undefined;
+    };
+  };
+  img3: {
+    childImageSharp: {
+      fluid?: FluidObject | FluidObject[] | undefined;
+    };
+  };
+  // images:
 }
 
 const FoodGrid: React.FC<Props> = () => {
-  const data: Props = useStaticQuery(queryData);
-  // console.log('img', data);
+  const data: Props = useStaticQuery(SINGLE_IMG);
+  console.log('img', data);
   return (
     <StyledFoodGrid>
       <GridItem className="main">
         <Img fluid={data.img.childImageSharp.fluid} />
+        <p>Ramen Noodles</p>
+      </GridItem>
+      <GridItem className="main">
+        <Img fluid={data.img2.childImageSharp.fluid} />
+        <p>Pizza</p>
+      </GridItem>
+      <GridItem className="main">
+        <Img fluid={data.img3.childImageSharp.fluid} />
+        <p>Ice cream</p>
       </GridItem>
     </StyledFoodGrid>
   );
--- src/components/home/FoodGrid/Styles.foodgrid.tsx
+++ src/components/home/FoodGrid/Styles.foodgrid.tsx
@@ -1,9 +1,75 @@
 import styled from 'styled-components';
 
 export const StyledFoodGrid = styled.div`
-  /* display: flex; */
-  /* flex-wrap: wrap; */
+  display: grid;
+  grid-template-columns: auto;
+  grid-row-gap: 1rem;
   margin: 2rem 0;
+
+  @media (min-width: 576px) {
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 1rem;
+  }
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+    grid-column-gap: 1rem;
+  }
+  @media (min-width: 992px) {
+    .gatsby-image-wrapper {
+      height: 100% !important;
+    }
+
+    grid-template-areas:
+      'one one two two'
+      'one one three three';
+  }
+  .gatsby-image-wrapper {
+    height: 100% !important;
+  }
 `;
 
-export const GridItem = styled.div``;
+export const GridItem = styled.div`
+  position: relative;
+  transition: ${({ theme }) => theme.transition.quickTransition};
+
+  @media (min-width: 992px) {
+    &:nth-child(1) {
+      grid-area: one;
+    }
+    &:nth-child(2) {
+      grid-area: two;
+    }
+    &:nth-child(3) {
+      grid-area: three;
+    }
+  }
+
+  p {
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: ${({ theme }) => theme.colors.common};
+    background: ${({ theme }) => theme.colors.darkRGBA};
+    padding: 0.1rem 0.3rem;
+    text-transform: capitalize;
+    z-index: 3;
+    display: none;
+  }
+
+  &:after {
+    content: '';
+    background: rgba(0, 0, 0, 0.3);
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  cursor: pointer;
+  &:hover {
+    p {
+      display: block;
+    }
+  }
+`;
