c6e591af00d321c59d2c6430d9ce4960cfd00a19
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-02-21 15:40:10 +0100

    Add welcome and userById handlers to main.go

--- auth/main.go
+++ auth/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "net/http"
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to the home page!"))
+}
+
+func userById(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	w.Write([]byte("User with id: " + id))
+}
+
 func main() {
 
+	http.HandleFunc("GET /", welcome)
+	http.HandleFunc("GET /users/{id}", userById)
+
+	http.ListenAndServe(":8080", nil)
 }
