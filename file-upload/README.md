51e4651507ac160335bbbd08976f3e162b148881
masiucd <ciszekmarcell@gmail.com>
2024-04-20 18:51:25 +0200

    Add file upload feature and view uploads functionality

---
+++ file-upload/README.md
@@ -0,0 +1,10 @@
+# File upload
+
+## About
+
+This is a simple file upload application that allows users to upload files to the server.
+And then view the uploaded files.
+
+### Notes
+
+- The form attribute `enctype="multipart/form-data"` is required when you are using forms that have a file upload control. Without this attribute, the file upload will not work with binary files only text files.
--- file-upload/main.go
+++ file-upload/main.go
@@ -1,17 +1,103 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"text/template"
+)
+
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+const uploadPath = "uploads"
+const maxUploadSize = 10 << 20 // 10MB
 
 func main() {
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /test", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
-	})
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("GET /upload", uploadView)
+	mux.HandleFunc("POST /upload", upload)
 
-	mux.HandleFunc("GET /task/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		w.Write([]byte("Task ID: " + id))
-	})
+	err := http.ListenAndServe(":9000", mux)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
-	http.ListenAndServe(":8080", mux)
+func uploadView(w http.ResponseWriter, r *http.Request) {
+	type pageData struct {
+		Title string
+	}
+	template := template.Must(template.ParseFiles("upload.html"))
+	data := pageData{
+		Title: "File Upload",
+	}
+	template.Execute(w, data)
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+
+	// parse the form data
+	// 10 << 20 = 10 * 2^20 = 10 * 1024 * 1024 = 10MB. Max file size
+	r.ParseMultipartForm(maxUploadSize)
+
+	// get the file from the form
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	// Create a new file in the upload directory
+	dst, err := os.Create(filepath.Join(uploadPath, handler.Filename))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("Successfully Uploaded File")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	// read the files in the uploads directory
+	type PageData struct {
+		Title string
+		Files []os.DirEntry
+	}
+	files, err := os.ReadDir(uploadPath)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	data := PageData{
+		Title: "View Uploads",
+		Files: files,
+	}
+
+	template := template.Must(
+		template.ParseFiles("view-uploads.html"),
+	)
+	template.Execute(w, data)
+	// files, err := os.ReadDir(uploadPath)
 }
