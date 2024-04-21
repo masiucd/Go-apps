20b126bd2760407d632bcd0f473fb5ec728866dd
masiucd <ciszekmarcell@gmail.com>
2024-04-21 09:21:13 +0200

    Update go.mod and go.sum files to include new dependencies

--- file-upload2.0/go.mod
+++ file-upload2.0/go.mod
@@ -2,10 +2,14 @@ module go-apps.com/file-upload2.0
 
 go 1.22.2
 
+require github.com/gofiber/template/html/v2 v2.1.1
+
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/gofiber/fiber/v2 v2.52.4 // indirect
 	github.com/gofiber/storage/sqlite3/v2 v2.1.1 // indirect
+	github.com/gofiber/template v1.8.3 // indirect
+	github.com/gofiber/utils v1.1.0 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
--- file-upload2.0/src/main.go
+++ file-upload2.0/src/main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 	"go-apps.com/file-upload2.0/src/db"
 )
 
 const port = ":8000"
 
 func main() {
-	app := fiber.New()
+	engine := html.New("./src/views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+		// ViewsLayout: "layouts/main",
+	})
 	db.InitDB()
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.Render("index",
+			fiber.Map{
+				"Title": "File Upload",
+			},
+		)
+	})
+
+	app.Get("/upload", func(c *fiber.Ctx) error {
+		return c.Render("upload",
+			fiber.Map{
+				"Title": "Upload File",
+			},
+		)
+
 	})
 
 	err := app.Listen(port)
