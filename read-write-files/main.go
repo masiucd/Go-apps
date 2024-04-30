818625c82bfbfc4dd3ca27d85e712be563e9d62b
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-04-30 10:24:29 +0200

    Refactor file reading and printing logic in main.go

--- read-write-files/main.go
+++ read-write-files/main.go
@@ -5,11 +5,21 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 // 10 is the ASCII code for newline
 func main() {
-	file, err := os.Open("file.txt")
+
+	data := readFileV1("file.txt")
+	simplePrint(data)
+	printEven(data)
+
+}
+
+// readFileV1 reads the file and returns the data as a byte slice
+func readFileV1(f string) []byte {
+	file, err := os.Open(f)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -18,8 +28,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	simplePrint(data)
-
+	return data
 }
 
 const newLine = 10
@@ -37,4 +46,27 @@ func simplePrint(data []byte) {
 		}
 	}
 	fmt.Println(output)
+}
+
+func printEven(data []byte) {
+	var builder strings.Builder
+	lineCount := 0
+	for _, v := range data {
+		if v == '\n' {
+			// If it's an even line, print it
+			if lineCount%2 == 0 {
+				fmt.Println(builder.String())
+			}
+			// Reset the builder and increase the line count
+			builder.Reset()
+			lineCount++
+		} else {
+			// Add the character to the current line
+			builder.WriteByte(v)
+		}
+	}
+	// Print the last line if it's an even line and the data doesn't end with a newline
+	if lineCount%2 == 0 {
+		fmt.Println(builder.String())
+	}
 }
