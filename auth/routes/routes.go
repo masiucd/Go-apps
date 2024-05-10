5812b5ca41bd1ed3e9a55d697a148e68ea5d7b46
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-05-10 17:21:34 +0200

    refactor: Parse form data and extract user input in CreateUser function

--- auth/routes/routes.go
+++ auth/routes/routes.go
@@ -1,20 +1,18 @@
 package routes
 
 import (
-	"go-apps/auth.com/api"
 	routes "go-apps/auth.com/routes/user"
 	"net/http"
 )
 
 func Init() *http.ServeMux {
 	mux := http.NewServeMux()
-	// mux.Handle("/", fs)
 
 	mux.HandleFunc("GET /users/{id}", routes.UserById)
 	mux.HandleFunc("GET /users", routes.UserById)
 	mux.HandleFunc("GET /signup", routes.SignUp)
 	mux.HandleFunc("GET /login", routes.Login)
-	mux.HandleFunc("POST /create-user", api.CreateUser)
+	mux.HandleFunc("POST /create-user", routes.CreateUser)
 
 	return mux
 }
--- auth/routes/user/users.go
+++ auth/routes/user/users.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"go-apps/auth.com/input"
 	"go-apps/auth.com/lib"
 	"go-apps/auth.com/persistence"
 	"net/http"
@@ -47,4 +48,34 @@ func SignUp(w http.ResponseWriter, r *http.Request) {
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	lib.ExecuteTemplate("login", w)
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+
+	r.ParseForm()
+
+	formValue := getFormValue(r)
+	input := input.UserInput{
+		FirstName: formValue("firstname"),
+		LastName:  formValue("lastname"),
+		Email:     formValue("email"),
+		Password:  formValue("password"),
+	}
+
+	error := persistence.InsertUser(input)
+	if error != nil {
+		http.Error(w, error.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/login", http.StatusMovedPermanently)
+
+}
+
+func getFormValue(r *http.Request) func(key string) string {
+	return func(key string) string {
+		if val, ok := r.Form[key]; ok {
+			return val[0]
+		}
+		return ""
+	}
 }
