be64275c18d790320748791c132ec3cbfba39d59
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-06-11 09:28:42 +0200

    refactor: Optimize code for reading and plotting injury statistics data

---
+++ injury-statistics/README.md
@@ -0,0 +1,9 @@
+# Injury statistics
+
+## About
+
+Reading CSV files with data about injuries and plotting the data.
+
+Since the data are large we want to optimize the code to be able to read and plot the data in a reasonable time.
+
+The CSV files are publicly available at [https://www.cdc.gov/injury/wisqars/](https://www.cdc.gov/injury/wisqars/)
--- injury-statistics/main.go
+++ injury-statistics/main.go
@@ -1,3 +1,82 @@
 package main
 
-func main() {}
+import (
+	"github.com/fatih/color"
+	"github.com/rodaine/table"
+)
+
+var testData = [][]string{
+	{"1", "Alice", "100", "2018-01-01"},
+	{"2", "Bob", "95", "2018-01-02"},
+	{"3", "Charlie", "90", "2018-01-03"},
+	{"4", "David", "85", "2018-01-04"},
+}
+
+func main() {
+	headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
+	columnFmt := color.New(color.FgYellow).SprintfFunc()
+
+	tbl := table.New("ID", "Name", "Score", "Added")
+	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+
+	for _, row := range testData {
+		tbl.AddRow(row[0], row[1], row[2], row[3])
+	}
+	tbl.Print()
+
+	// file, err := os.Open("file-1.csv")
+	// if err != nil {
+	// 	fmt.Println("Error: ", err)
+	// }
+	// defer file.Close()
+
+	// reader := csv.NewReader(file)
+	// reader.FieldsPerRecord = -1 // Accept variable number of fields per line
+	// data, err := reader.ReadAll()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// var titles []string
+	// for _, title := range data[0] {
+	// 	titles = append(titles, title)
+	// }
+
+	// var result []map[string]string
+	// fmt.Println(len(titles))
+	// fmt.Println(len(data[2]))
+	// for i := range data[1:] {
+	// 	// item := make(map[string]string)
+	// 	for _, col := range titles {
+	// 		fmt.Println(titles[i], col)
+	// 	}
+	// 	// result = append(result, item)
+	// }
+
+	// fmt.Println(result)
+	// var xs []string
+	// for _, row := range data {
+
+	// 	for _, col := range row {
+	// 		fmt.Println(col)
+	// 	}
+
+	// }
+
+	// fmt.Println(injuryItems)
+}
+
+// type InjuryItem struct {
+// 	SeriesReference string `csv:Series_reference`
+// 	Period          string `csv:Period`
+// 	Type            string `csv:Type`
+// 	DataValue       string `csv:Data_value`
+// 	Lower_CI        string `csv:Lower_CI`
+// 	Units           string `csv:Units`
+// 	Indicator       string `csv:Indicator`
+// 	Cause           string `csv:Cause`
+// 	Validation      string `csv:Validation`
+// 	Population      string `csv:Population`
+// 	Age             string `csv:Age`
+// 	Severity        string `csv:Severity`
+// }
