ead922f444b8392e482a18d1eff78c83232c3d42
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-07-04 10:00:09 +0200

    refactor: Update Dockerfile and Go app to use Go version 1.22

--- go-docker/main.go
+++ go-docker/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/repeale/fp-go"
+)
+
+func isGreaterThanOne(n int) bool {
+	return n > 1
+}
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World! from Go using Docker!"))
+	})
+	fn := fp.Filter(isGreaterThanOne)
+	res := fn([]int{1, 2, 3, 4, 5})
+	fmt.Println(res)
 	fmt.Println("Hello World! from Go using Docker!")
+	http.ListenAndServe(":3000", nil)
 }
